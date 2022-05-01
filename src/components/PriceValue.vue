965fdde472af8113a1d79b1afa5ffca15507ad06
Dmitry Stoilik <dmitry.stoilik@intexsoft.by>
2022-05-01 23:28:16 +0300

    feat: add value formatting

--- src/components/PriceValue.vue
+++ src/components/PriceValue.vue
@@ -1,12 +1,14 @@
 <template>
   <input
-    :value="price"
+    :value="formattedPrice"
     @input="input($event.target.value)"
     type="number"
     min="0"
     placeholder="value"
     class="price-value"
+    :title="price"
     @keydown="checkInputKey"
+    @focus="isFocused = true"
     @blur="blur"
   />
 </template>
@@ -19,14 +21,28 @@ const RESTRICTED_KEYS = ['-', 'e'];
 @Component
 export default class PriceValue extends Vue {
   @VModel({ type: Number }) price!: number;
+
+  isFocused = false;
+
   @Emit()
   blur() {
+    this.isFocused = false;
     return;
   }
 
   @Emit()
   input(value: string) {
     return parseFloat(value) || 0;
+  }
+
+  get formattedPrice() {
+    return this.isFocused
+      ? this.price
+      : this.price.toFixed(this.decimalDigitsCount);
+  }
+
+  get decimalDigitsCount() {
+    return this.price.toString().split('.')[1]?.length > 2 ? 2 : 1;
   }
 
   checkInputKey(event: KeyboardEvent) {
